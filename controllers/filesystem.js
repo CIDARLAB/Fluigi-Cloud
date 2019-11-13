fb213625b53943407ff714bc21ad899bdbc2b3c5
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2019-11-13 16:52:48 -0500

    Created a test api that controls the download

---
+++ controllers/filesystem.js
@@ -0,0 +1,38 @@
+
+var File = require('../models/file');
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+
+module.exports.downloadFile = function(req, res) {
+    // var fileid = req.query.id;
+    var fileid = "5dc2505f9d8bd6543091b665";
+    if (null == fileid) { res.sendStatus(400) }
+    console.log("requesting file id: " + fileid);
+
+    // res.send({ id: data._id, name: data.name, ext: data.file_extension, link: data.S3_path });
+    // res.attachment("data.name");
+    // s3.getObject({
+    //     Bucket:"neptune-storage",
+    //     Key: "5dc1f35adff8e995f7da4696"                  //data.id
+    // }).createReadStream().pipe(res);
+
+    File.findById(fileid, function(err, data) {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+        if (null == data) {
+            res.sendStatus(400);
+            console.log("Cannot find data with given fileid: " + fileid);
+            return;
+        }
+        // res.send({ id: data._id, name: data.name, ext: data.file_extension, link: data.S3_path });
+        res.attachment(data.name);
+        s3.getObject({
+            Bucket: process.env['NEPTUNE_S3_BUCKET_ID'],
+            Key: "5dc2505f9d8bd6543091b665"                  //data.id
+        }).createReadStream().pipe(res);
+    });
+
+};
