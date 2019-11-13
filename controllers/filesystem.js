a8a079b77e7813ceef3bab3d586003caf7b87f10
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2019-11-13 17:10:58 -0500

    Updated the method to retrieve the files correctly.

--- controllers/filesystem.js
+++ controllers/filesystem.js
@@ -5,8 +5,8 @@ var s3 = new AWS.S3();
 
 
 module.exports.downloadFile = function(req, res) {
-    // var fileid = req.query.id;
-    var fileid = "5dc2505f9d8bd6543091b665";
+    var fileid = req.query.id;
+    // var fileid = "5dc2505f9d8bd6543091b665";
     if (null == fileid) { res.sendStatus(400) }
     console.log("requesting file id: " + fileid);
 
@@ -28,11 +28,12 @@ module.exports.downloadFile = function(req, res) {
             return;
         }
         // res.send({ id: data._id, name: data.name, ext: data.file_extension, link: data.S3_path });
+        // console.log({ id: data._id, name: data.name, ext: data.file_extension, link: data.S3_path });
         res.attachment(data.name);
         s3.getObject({
             Bucket: process.env['NEPTUNE_S3_BUCKET_ID'],
-            Key: "5dc2505f9d8bd6543091b665"                  //data.id
-        }).createReadStream().pipe(res);
+            Key: fileid                  //data.id
+        }).createReadStream().pipe(res);;
     });
 
 };
